686b472171d57cd83eef596be4c208855cdcbe4d
Malik <malik@instamotor.com>
2014-08-15 14:53:02 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '65d1a8513304e81771c71e90ec4c0c31c7a5605bb7ba2238108b1e10ae6d7e35b9e6728a1bdf9af3e7d3cebc2b118346668368df9bc09866f9d73851a3de369b'
